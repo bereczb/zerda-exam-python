e342b3da1219b69b41bc9d8b6c1a12cc9d6fbc74
bereczb <balazs.berecz@gmail.com>
2016-12-05 09:49:16 +0100

    second_task_completed

--- second.py
+++ second.py
@@ -4,3 +4,17 @@
 # If any problem raises with the file output, the function should not break, but return False.
 # Example: when called with the following two parameters: "tree.txt", "apple",
 # the function should write "appleappleapple" to the file "tree.txt", and return True.
+
+def write_to_file(name_of_file, string_to_write):
+
+    try:
+        file_work_with = open(name_of_file, 'w')
+        for i in range(10):
+            file_work_with.write(string_to_write)
+        file_work_with.close()
+        return True
+
+    except:
+        return False
+
+print(write_to_file('tree.txt', 'apple'))
